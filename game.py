91eb0c741c081b8245a66c5eb4c0f173d86678f0
Bismay7978 <bismayrana@gmail.com>
2021-02-05 22:25:24 +0530

    first commit

--- game.py
+++ game.py
@@ -1,37 +1,43 @@
-from player import Human_player, Computer
+from player import Human_player, Computer, SuperComputer
 from os import system
+from random import choice
+from time import sleep
 
 
 class tictactoe:
     def __init__(self):
         self.bord = [' ' for _ in range(9)]
         self.current_winer = None
-        self.available_moves = [i+1 for i, _ in enumerate(self.bord)]
+
+    def available_moves(self):
+        # return a list of available moves
+        return [i+1 for i, _ in enumerate(self.bord) if _ == ' ']
 
     def playing_again(self):
         self.bord = [' ' for _ in range(9)]
         self.current_winer = None
-        self.available_moves = [i+1 for i, _ in enumerate(self.bord)]
 
-    def print_bord(self):
+    def print_bord(self):  # printing the game bord
         for row in [self.bord[j*3:(j+1)*3] for j in range(3)]:
             print('| '+str.join(' | ', row)+' |')
 
     @staticmethod
-    def print_num_bord():
+    def print_num_bord():  # printing the number bord for help in identify the square
         for row in [[str(i+1) for i in range(j*3, (j+1)*3)] for j in range(3)]:
             print('| '+str.join(' | ', row)+' |')
 
-    def check_winer(self, square: int, later: str):
+    def check_winer(self, square: int, later: str):  # To check the winner of the game
         row_index = (square-1)//3
         row = self.bord[row_index*3:(row_index+1)*3]
+        # if any row have laters are same as current mover then current mover is winner
         if all([(later == row_item) for row_item in row]):
             return True
         column_index = (square-1) % 3
         column = [self.bord[column_index+(i*3)] for i in range(3)]
+        # if any column have laters are same as current mover then current mover is winner
         if all([(later == column_item) for column_item in column]):
             return True
-        if (square-1) % 2 == 0:
+        if (square-1) % 2 == 0:  # here we check for diagonal for evaluat the winner
             l_diagonal = [self.bord[4*i] for i in range(3)]
             if all([later == l_diagonal_item for l_diagonal_item in l_diagonal]):
                 return True
@@ -41,55 +47,108 @@ class tictactoe:
         return False
 
     def num_of_available_moves(self):
-        return len(self.available_moves)
+        return len(self.available_moves())
 
-    def make_move(self, square: int, plr):
+    def make_move(self, square, later):
         if self.bord[square-1] == ' ':
-            self.bord[square-1] = plr.later.upper()
-            self.available_moves.remove(square)
-            if self.check_winer(square, plr.later):
-                self.current_winer = plr.name
+            self.bord[square-1] = later.upper()
+            if self.check_winer(square, later):
+                self.current_winer = later
             return True
         else:
             print("The move has been used try another available move")
 
         return False
 
-
-def play(game, x_player, y_player, print_bord=True):
-    system('cls')
-    plr = x_player
-    game.print_num_bord()
-    while game.num_of_available_moves():
+    def play(self, x_player, o_player, print_bord=True):
+        system('cls')
+        plr = choice((x_player, o_player))
         if plr == x_player:
-            square = x_player.get_move(game)
+            print(f"The {x_player.name} going to move first")
         else:
-            square = y_player.get_move(game)
-        if game.make_move(square, plr):
-            if print_bord:
-                system('cls')
-                print(f"{plr.name} make move to {square}")
-                game.print_num_bord()
-                print()
-                game.print_bord()
-        else:
-            continue
-        if game.current_winer:
-            print(f"The {game.current_winer} win the match")
-            return
-        plr = x_player if plr == y_player else y_player
-    if not game.current_winer:
-        print("The game is tie")
+            print(f"The {o_player.name} going to move first")
+        sleep(2)
+        system('cls')
+        self.print_num_bord()
+        while self.num_of_available_moves():
+            if plr == x_player:
+                square = x_player.get_move(self)
+            else:
+                square = o_player.get_move(self)
+            if self.make_move(square, plr.later):
+                if print_bord:
+                    system('cls')
+                    print(f"{plr.name} make move to {square}")
+                    self.print_num_bord()
+                    print()
+                    self.print_bord()
+            else:
+                continue
+            if self.current_winer:
+                print(
+                    f"The {x_player.name if x_player.later == self.current_winer else o_player.name} win the match")
+                return
+            plr = x_player if plr == o_player else o_player
+        if not self.current_winer:
+            print("The game is tie")
+
+
+def setup_player(name, symbol):
+    x_player = object
+    o_player = object
+    print('Choose other player from this list of player')
+    print("1.Basic Computer\n2.AI Computer\n3.Human\n4.Exit")
+    is_valid_choice = False
+    while not is_valid_choice:
+        choice = input("Enter your choice\n")
+        try:
+            choice = int(choice)
+            if choice == 1:
+                is_valid_choice = True
+                if symbol == 'X':
+                    x_player = Human_player(symbol, name)
+                    o_player = Computer('O')
+                else:
+                    x_player = Computer('X')
+                    o_player = Human_player(symbol, name)
+            elif choice == 2:
+                is_valid_choice = True
+                if symbol == 'X':
+                    x_player = Human_player(symbol, name)
+                    o_player = SuperComputer('O')
+                else:
+                    x_player = SuperComputer('X')
+                    o_player = Human_player(symbol, name)
+            elif choice == 3:
+                is_valid_choice = True
+                other_player_name = input("Enter the name of other player\n")
+                if symbol == 'X':
+                    x_player = Human_player(symbol, name)
+                    o_player = Human_player('O', other_player_name)
+                else:
+                    x_player = Human_player('X', other_player_name)
+                    o_player = Human_player(symbol, name)
+            elif choice == 4:
+                exit(0)
+            else:
+                print("Please choose from the option")
+        except ValueError:
+            print('Invalid input')
+    return x_player, o_player
 
 
 if __name__ == '__main__':
-    t = tictactoe()
+    t = tictactoe()  # create an instance of tictactoe class
     name = input("Enter your good name\n")
-    x_player = Human_player('X', name)
-    y_player = Computer('O', 'Computer')
+    symbol = input("Enter your symbol ( X | O )\n").upper()
+    while not (symbol.isalpha() and (symbol == 'X' or symbol == 'O')):
+        print("You have enter incorrect symbol please try agin")
+        symbol = input("Enter your symbol ( X | O )\n").upper()
     while True:
-        play(t, x_player, y_player)
-        play_again = input("You want to play again (Y/N)\n")
+        x_player, o_player = setup_player(name, symbol)
+        t.play(x_player, o_player)
+        play_again = input(
+            "You want to play again press 'Y' or to exit press any key\n")
         if play_again.upper() == 'Y':
             t.playing_again()
         else:
--- player.py
+++ player.py
@@ -1,36 +1,39 @@
 import random
 from time import sleep
+import math
 
 
 class Player:
-    def __init__(self, later, name):
+    def __init__(self, later, name, player_ID):
         self.later = later
         self.name = name
+        self.player_ID = player_ID
 
     def get_move(self, game):
         pass
 
 
 class Computer(Player):
-    def __init__(self, later, name):
-        super().__init__(later, name)
+
+    def __init__(self, later, name='Basic Computer'):
+        super().__init__(later, name, 1)
 
     def get_move(self, game):
         sleep(1.5)
-        return random.choice(game.available_moves)
+        return random.choice(game.available_moves())
 
 
 class Human_player(Player):
     def __init__(self, later, name):
-        super().__init__(later, name)
+        super().__init__(later, name, 2)
 
     def get_move(self, game):
         val = None
         # moves = [i+1 for i in game.available_moves]
         while not val:
-            print("Your symbol is X")
+            print(f"Your symbol is {self.later}")
             val = input(
-                f"Choose {self.name} from this available moves {game.available_moves}\n")
+                f"Choose {self.name} from this available moves {sorted(game.available_moves())}\n")
             try:
                 val = int(val)
                 if val < 1 or val > 9:
@@ -39,3 +42,50 @@ class Human_player(Player):
                 print("Invalid input try again")
                 val = None
         return val
+
+
+class SuperComputer(Player):
+
+    def __init__(self, later, name='AI Computer'):
+        super().__init__(later, name, 3)
+
+    # it is a MINMAX algorithm which maximizes the wining chance of Super computer while minimizes the wining chance other player
+    def perfect_move(self, game, player):
+        other_player = 'X' if player == 'O' else 'O'
+        # base case if we have the current winer then or the game tie then it return the associate score
+        if game.current_winer:
+            # the score for winer identifies that if the player is super computer then it is positive else for other player it gives negative value. The main use is to calculate the best move for the super computer
+            return {'position': None, 'score': game.num_of_available_moves()+1 if game.current_winer == self.later else -1*(game.num_of_available_moves()+1)}
+        elif not game.num_of_available_moves():
+            # if the game is tai then return score 0 for all player
+            return {'position': None, 'score': 0}
+        # initialize the best move for different player
+        if player == self.later:
+            # if the player is super computer then best move should be maximize for increase the chance of wining the super computer
+            best_move = {'position': None, 'score': -math.inf}
+        else:
+            # if the player is other than super computer then best move should be minimize for increase the chance of wining the super computer
+            best_move = {'position': None, 'score': math.inf}
+        for move in game.available_moves():
+            # make a posible move form available move by current player
+            game.make_move(move, player)
+            # recursivly call the method to calculate the best move of both player
+            temp_move = self.perfect_move(game, other_player)
+            temp_move['position'] = move
+            # finding the best move for both player
+            # it checks best move for super computer by checking max score
+            if player == self.later and temp_move['score'] > best_move['score']:
+                best_move = temp_move
+            # it checks best move for other player by checking min score
+            elif player != self.later and temp_move['score'] < best_move['score']:
+                best_move = temp_move
+            # reset all moves(Back tracking)
+            game.bord[move-1] = ' '
+            # reset current winer to none
+            game.current_winer = None
+        return best_move
+
+    def get_move(self, game):
+        if game.num_of_available_moves() == 9:
+            return random.choice(game.available_moves())
+        return self.perfect_move(game, self.later).get('position')
